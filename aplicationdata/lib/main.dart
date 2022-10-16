e1d1aa24e9c44298f75aaa0782a245cdf9c32621
Nestor <nestorrs2017@gmail.com>
2022-10-16 11:08:30 -0500

    Configuracion FlutterFIre

--- aplicationdata/lib/main.dart
+++ aplicationdata/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:aplicationdata/examples/to_do_firebase/my_app_to_doo.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() => runApp(new MyApp());
 
